b9b059a68d3a0c4e14eb919ce37c99fc3cffef33
Akechi Goro <zane41181@gmail.com>
2025-04-22 16:43:54 +0300

    new folders

---
+++ script.js
@@ -0,0 +1,20 @@
+const musicContainer = document.querySelector('.music-container')
+const playBtn=document.querySelector('#play')
+const prevBtn=document.querySelector('#prev')
+const nextBtn=document.querySelector('#next')
+const audio=document.querySelector('#audio')
+const progress=document.querySelector('.progress')
+const progressContainer=document.querySelector('.progress-container')
+const title = document.querySelector('#title')
+const cover = document.querySelector('#cover')
+
+
+
+///song titles
+
+const songs = ['Color Your Night', '1','2']
+
+// track of songs
+
+let songIndex = 2
+
